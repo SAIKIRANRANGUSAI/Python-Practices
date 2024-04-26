4be6c2a7bf532e4ae04bd309bfdb71bde2010413
SAIKIRANRANGUSAI <saikiranrangu6@gmail.com>
2024-04-26 23:16:05 +0530

    [new]

---
+++ oops in python/Inheritanc_e.py
@@ -0,0 +1,11 @@
+"""Inheritance: Inheritance allows us to define a class that inherits all the methods and properties from another class.
+-> Use super() in the child class to inherit all the properties and methods from the parent class.
+-> Inheritance can be classified into 5 types:
+   -> Single Inheritance: where one class inherits from another class.
+   -> Multiple Inheritance: where one class has more than one parent class.
+   -> Multilevel Inheritance: where a class inherits from a parent, and that parent class itself inherits from another class.
+        Example: a -> b -> c -> d
+   -> Hierarchical Inheritance: where more than one class inherits properties and behaviors from a single parent class.
+   -> Hybrid Inheritance: a combination of multiple and multilevel inheritance.
+        Example: a -> b -> c and a,c -> d
+"""
