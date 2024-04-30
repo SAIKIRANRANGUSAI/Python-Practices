362cc36eb4dfb5cda4ddad94380d268ba617a5e3
SAIKIRANRANGUSAI <saikiranrangu6@gmail.com>
2024-04-30 22:50:37 +0530

    adding new

---
+++ Api_S/a_p_i_using_flask.py
@@ -0,0 +1,7 @@
+"""
+
+-> render_template() is indeed used to return HTML pages dynamically by rendering templates.
+-> redirect is commonly used for redirecting users to another page or endpoint after submitting a form or performing some action.
+-> url_for() is used to generate URLs for different routes or endpoints in your Flask application,
+    allowing you to maintain flexibility and consistency in your code.
+"""
---
+++ Api_S/api_project.py
@@ -0,0 +1,45 @@
+from flask import Flask, redirect, url_for
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+app.secret_key = "sai"
+
+app.config["MYSQL_DB"] = "saikiran"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = "Sai@8978"
+app.config["MYSQL_HOST"] = "127.0.0.1"
+
+mysql = MySQL(app)
+
+@app.route("/")
+def create_table():
+    if mysql.connection:
+        sai = mysql.connection.cursor()
+        sai.execute("CREATE TABLE IF NOT EXISTS new_project ("
+                    "id INT AUTO_INCREMENT PRIMARY KEY,"
+                    "name VARCHAR(200),"
+                    "age INT,"
+                    "phonenumber BIGINT(13),"
+                    "address VARCHAR(190)"
+                    ")")
+        mysql.connection.commit()
+        sai.close()
+        return redirect(url_for("insert_data"))
+    else:
+        return "There is a connection problem, sorry!"
+
+@app.route("/insert")
+def insert_data():
+    if mysql.connection:
+        ssai = mysql.connection.cursor()
+        ssai.execute("INSERT INTO new_project (name, age, phonenumber, address) "
+                     "VALUES ('saikiran', 23, 8978553778, 'hyd')")
+        mysql.connection.commit()
+        ssai.close()
+        return "Data inserted successfully."
+    else:
+        return "There is a connection problem, sorry!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
